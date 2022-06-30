1e240d11089bd43f27adc568deb49cca037d9986
asaf-sh <asaf.shoham8@gmail.com>
2022-06-30 16:18:05 +0300

    malloc3

--- malloc_3.cpp
+++ malloc_3.cpp
@@ -3,8 +3,10 @@
 #include <unistd.h>
 #include <string.h>
 
+#include <sys/mman.h>
+
 #define MAX_SIZE pow(10, 8)
-#define BAD_SBRK ((void*)(-1))
+#define BAD_ALLOC ((void*)(-1))
 
 struct MallocMetaData_t;
 typedef struct MallocMetaData_t* MD;
@@ -13,10 +15,16 @@ struct MallocMetaData_t{
     MD prev;
     MD next;
     bool is_free;
+    bool is_heap;
     size_t size;
+    MD adjacent_prev;
 };
 
+#define Ceil8(x) ((x%8 ? x+8-%8 : x))
 #define MD_SIZE (sizeof(struct MallocMetaData_t))
+#define MD_8SIZE (Ceil8(MD_SIZE))
+#define MMAP_SIZE (128 * 1024)
+#define SPLIT_SIZE (128 + MD_8SIZE)
 
 //*********START - GENERAL UTILS************//
 bool validate_size(size_t size){
@@ -24,11 +32,11 @@ bool validate_size(size_t size){
 }
 
 void* md2d(MD md){
-    return (void*) (md+MD_SIZE);
+    return (void*) (md+MD_8SIZE);
 }
 
 MD d2md(void* d){
-    return (MD)d - MD_SIZE;
+    return (MD)d - MD_8SIZE;
 }
 
 
@@ -37,11 +45,11 @@ MD d2md(void* d){
 //*********START - LIST************//
 
 struct MallocMetaData_t head_t = {&head_t, &head_t, false, 0};
-//head_t.next = &head_t;
-//head_t.prev = &head_t;
 MD head = (MD) &head_t;
-//head->prev = head;
-//head->next = head;
+
+MD ncc1701d = NULL;
+MD elizabeth2 = NULL;
+
 
 bool is_empty(){
     return head->next == head;
@@ -55,13 +63,16 @@ void _insert_before(MD new_md, MD pos){
     new_md->next = pos;
 }
 
+void _remove(MD md){
+    md->prev->next = md->next;
+    md->next->prev = md->prev;
+}
+
 void push_back(MD md){
     _insert_before(md, head);
 }
 
-void insert(MD md){
-    push_back(md);
-}
+
 
 typedef bool (*BlockFilter) (MD, void*);
 
@@ -71,30 +82,79 @@ MD get_first(BlockFilter filter, void* args){
     return (curr == head ? NULL: curr);
 }
 
+struct lex_key{
+    size_t size;
+    void* addr;
+}
+typedef struct lex_key* LK;
+
+bool size_x_addr_lex(MD md, void* void_key){
+    LK key = (LK) void_key;
+    return md->size > key->size || (md->size == key->size && md > key->addr);
+}
+
+void insert(MD md){
+    lex_key md_key = {md->size, md};
+    MD first_greater = get_first(size_x_addr_lex, &md_key);
+    first_greater ? _insert_before(md, first_greater) : push_back(md);
+}
+
 //*********END - LIST************//
 
 
 //*********START - ALLOC UTILS************//
 
-//calls sbrk for new allocation (including its metadata), and add its metadata to end of list
-//similar code to smalloc from malloc_1.cpp
+//for sure - size%8==0
 void* _new_assign(size_t size){
-    size_t full_size = size + MD_SIZE;
-    void* ppd = sbrk(full_size);
-    if (ppd == BAD_SBRK)
+    size_t full_size = size + MD_8SIZE;  //8 multiplicant
+
+    //wilderness expantion
+    if(full_size < MMAP_SIZE && ncc1701d && ncc1701d->is_free){
+        if(sbark(size - ncc1701d->size) == BAD_ALLOC)
+            return NULL;
+        ncc1701d->size = size;
+        return ncc1701d;
+    }
+
+    MD new_md = (full_size < MMAP_SIZE ? sbrk(full_size - (ncc1701d->is_free * ncc1701d->size)) : \
+                    mmap(NULL, full_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+    
+    if (new_md == BAD_ALLOC)
         return NULL;
     
-    MD new_md = (MD) ppd;
     new_md->size = size;
     new_md->is_free = false;
-    insert(new_md);
+    new_md->is_heap = (full_size < MMAP_SIZE);
 
+    if(new_md->is_heap){
+        new_md->adjacent_prev = ncc1701d;
+        ncc1701d = new_md;  //setting new frontier
+        insert(new_md);
+    }
+    
     return md2d(new_md);    
 }
 
-void* _re_assign(MD old_md){
+void split_block(MD old_md, size_t offset){
+    MD new_md = (old_md + MD_8SIZE) + offset;
+    new_md->is_free = true;
+    new_md->size = old_md->size - (offset + MD_8SIZE);
+    new_md->is_heap = true;
+    new_md->adjacent_prev = old_md;
+    insert(new_md);
+    
+    old_md->size = offset;
+    
+    if(old_md == ncc1701d)
+        ncc1701d = new_md;
+}
+
+void* _re_assign(MD old_md, size_t size){
     //old_md->size = size;  (md->size should not be changed - mentioned in WHW4)
     old_md->is_free = false;
+    if(old_md->size - size > SPLIT_SIZE)
+        split_block(old_md, size);
+
     return md2d(old_md);
 }
 
@@ -108,11 +168,13 @@ bool free_n_fit(MD md, void* size_ptr){
 //*********START - REQUSTED FUNCTIONS************//
 
 void* smalloc(size_t size){
+    size = Ceil8(size);
     if(!validate_size(size))
         return NULL;
-
     MD first_fit = get_first(free_n_fit, &size);
-    return (first_fit ? _re_assign(first_fit) : _new_assign(size));
+    
+    // notice that for size > MMAP first_fit must return as NULL
+    return first_fit ? _re_assign(first_fit, size) : _new_assign(size);
 }
 
 void* scalloc(size_t num, size_t size){
@@ -123,10 +185,48 @@ void* scalloc(size_t num, size_t size){
     return allocated_block;
 }
 
+void free_map(MD md){
+    if(munmap(p, p_md->size + MD_8SIZE) == -1)
+        printf("handle bad munmap");
+}
+
+MD get_next_adjacent(MD md){
+    return md == ncc1701d ? NULL : md + MD_8SIZE + md->size;
+}
+
+MD get_prev_adjacent(MD md){
+    return md->adjacent_prev;
+}
+
+MD merge(MD left, MD right){
+    _remove(right);
+    left->size += right->size + MD_8SIZE;
+    
+    if(right == ncc1701d)
+        ncc1701d = left;
+    
+    return left;
+}
+
+void free_heap(MD md){
+    md->is_free = true;
+    MD next = get_next_adjacent(md);
+    if(next && next->is_free)
+        md = merge(md, next);
+    
+    MD prev = get_prev_adjacent(md);
+    if(prev && prev->is_free)
+        md = merge(prev, md);
+    
+    _remove(md);
+    insert(md);
+}
+
 void sfree(void* p){
-    MD p_md;
-    if(p && !(p_md = d2md(p))->is_free)
-        p_md->is_free = true;
+    if(p){
+        MD p_md = (MD)p;
+        (MD)p->is_heap ? free_heap(p_md) : free_map(p_md);
+    }
 }
 
 void* srealloc(void* oldp, size_t size){
